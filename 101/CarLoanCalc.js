e96774540dd8a1169c7f811d90e9d6200e383197
Marek Bajsarowicz <keramjs@gmail.com>
2022-01-30 11:12:21 +0100

    loneCalc  update

--- 101/CarLoanCalc.js
+++ 101/CarLoanCalc.js
@@ -1,39 +1,38 @@
-
 const readline = require('readline-sync');
 
 function isValid(dataType) {
-    switch (dataType) {
-        case "loneAmount":
-            while (true) {
-                let loneAmount = Number(readline.question("Please write loan amount "));
-                if (loneAmount > 0) {
-                    return loneAmount
-                } else {
-                    console.log("Write reasonable amount of loan - positive numbers only more than 0")
-                }
-            }
-            break;
-        case "apr":
-            while (true) {
-                let apr = Number(readline.question("Please write Anually Precentage Rate "))
-                if (apr > 0) {
-                    return apr
-                } else {
-                    console.log("Please use only positive numbers bigger than 0 ")
-                }
-            }
-            break;
-        case "loneDuration":
-            while (true) {
-                let loneDuration = parseInt(readline.question("Please write lone duration in years "))
-                if (loneDuration > 0) {
-                    return loneDuration
-                } else {
-                    console.log("Please use only positive integers bigger than 0")
-                }
-            }
-            break;
-    }
+  switch (dataType) {
+    case "loneAmount":
+      while (true) {
+        let loneAmount = Number(readline.question("Please write loan amount "));
+        if (loneAmount > 0) {
+          return loneAmount
+        } else {
+          console.log("Write reasonable amount of loan - positive numbers only more than 0")
+        }
+      }
+      break;
+    case "apr":
+      while (true) {
+        let apr = Number(readline.question("Please write Anually Precentage Rate "))
+        if (apr > 0) {
+          return apr
+        } else {
+          console.log("Please use only positive numbers bigger than 0 ")
+        }
+      }
+      break;
+    case "loneDuration":
+      while (true) {
+        let loneDuration = parseInt(readline.question("Please write lone duration in years "))
+        if (loneDuration > 0) {
+          return loneDuration
+        } else {
+          console.log("Please use only positive integers bigger than 0")
+        }
+      }
+      break;
+  }
 }
 
 
