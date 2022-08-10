81b56b904bc3111b590a1e37156951517ad7e18a
Marek Bajsarowicz <keramjs@gmail.com>
2022-08-10 08:11:24 +0200

    rps reformating

--- 120/rps.js
+++ 120/rps.js
@@ -1,9 +1,5 @@
-/**
- * Players picks rps and shows to each others to check who won. There are 2 types of player, human and computer, human picks rps by input. computer picks randomly. Winner is displayed.
- */
-const redline = require("readline-sync");
-
 function makePlayer(isHuman = true) {
+  const redline = require("readline-sync");
   return {
     isHuman: isHuman,
 
@@ -22,21 +18,29 @@ function makePlayer(isHuman = true) {
     }
   }
 }
+function gameEngine() {
+  return {
 
-function checkWin(pl1, pl2) {
-  if ((pl1 === 'r' && pl2 === 's') || (pl1 === 's' && pl2 === 'p') || (pl1 === 'p' && pl2 === 'r')) {
-    console.log(`player 1 picked ${pl1} and player 2 picked ${pl2}`);
-    console.log("player one won !");
-  } else if ((pl1 === 's' && pl2 === 'r') || (pl1 === 'p' && pl2 === 's') || (pl1 === 'r' && pl2 === 'p')) {
-    console.log(`player 1 picked ${pl1} and player 2 picked ${pl2}`);
-    console.log("player two won !");
-  } else {
-    console.log(`player 1 picked ${pl1} and player 2 picked ${pl2}`);
-    console.log("draw")
+    gameStart() {
+      pl1 = makePlayer().makeMove();
+      pl2 = makePlayer(isHuman = false).makeMove();
+      this.checkWin(pl1, pl2);
+    },
+
+    checkWin(pl1, pl2) {
+      if ((pl1 === 'r' && pl2 === 's') || (pl1 === 's' && pl2 === 'p') || (pl1 === 'p' && pl2 === 'r')) {
+        console.log(`player 1 picked ${pl1} and player 2 picked ${pl2}`);
+        console.log("player one won !");
+      } else if ((pl1 === 's' && pl2 === 'r') || (pl1 === 'p' && pl2 === 's') || (pl1 === 'r' && pl2 === 'p')) {
+        console.log(`player 1 picked ${pl1} and player 2 picked ${pl2}`);
+        console.log("player two won !");
+      } else {
+        console.log(`player 1 picked ${pl1} and player 2 picked ${pl2}`);
+        console.log("draw")
+      }
+    }
   }
 }
 
-let humanPlayer = makePlayer();
-let computerPLayer = makePlayer(isHuman = false);
-checkWin(humanPlayer.makeMove(), computerPLayer.makeMove());
-
+let engine = gameEngine();
+engine.gameStart();
