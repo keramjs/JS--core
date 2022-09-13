b8002c2e4aa6952652f4011167799fc566d77311
Marek Bajsarowicz <keramjs@gmail.com>
2022-09-13 20:55:53 +0200

    exercuces

--- 101/exercices/StringProcessing8/5.js
+++ 101/exercices/StringProcessing8/5.js
@@ -1,4 +1,15 @@
 /* Write a function that takes a string as an argument and returns that string with every lowercase letter changed to uppercase and every uppercase letter changed to lowercase. Leave all other characters unchanged. */
 
-swapCase('CamelCase');              // "cAMELcASE"
-swapCase('Tonight on XYZ-TV');      // "tONIGHT ON xyz-tv"
+function swapCase(txt) {
+  let arr = txt.split(' ').map(element => {
+    if (element.length > 1) {
+      return element.split('').map(el => /[A-Z]/.test(el) ? el.toLowerCase() : el.toUpperCase())
+    } else {
+      return '';
+    }
+  });
+  return arr.map(element => element.join('')).join(' ')
+}
+
+console.log(swapCase('CamelCase'));              // "cAMELcASE"
+console.log(swapCase('Tonight on XYZ-TV'));      // "tONIGHT ON xyz-tv"
