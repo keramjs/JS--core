e21719c76da8e61a37b42419232689b20207615d
Marek Bajsarowicz <keramjs@gmail.com>
2022-09-18 13:49:15 +0200

    exercice

--- 101/exercices/StringProcessing8/6.js
+++ 101/exercices/StringProcessing8/6.js
@@ -1,5 +1,23 @@
 /* Write a function that takes a string as an argument and returns that string with a staggered capitalization scheme. Every other character, starting from the first, should be capitalized and should be followed by a lowercase or non-alphabetic character. Non-alphabetic characters should not be changed, but should be counted as characters for determining when to switch between upper and lower case. */
 
-staggeredCase('I Love Launch School!');        // "I LoVe lAuNcH ScHoOl!"
-staggeredCase('ALL_CAPS');                     // "AlL_CaPs"
-staggeredCase('ignore 77 the 4444 numbers');   // "IgNoRe 77 ThE 4444 nUmBeRs"
+function staggeredCase(txt) {
+  let arr = txt.split('');
+  return arr.map((el, idx) => {
+    if (idx % 2 !== 0) {
+      if (/[A-Z]/.test(el)) {
+        return el.toLowerCase();
+      } else {
+        return el;
+      }
+    } else {
+      if (/[a-z]/.test(el)) {
+        return el.toUpperCase();
+      } else {
+        return el;
+      }
+    }
+  }).join('')
+}
+console.log(staggeredCase('I Love Launch School!'));        // "I LoVe lAuNcH ScHoOl!"
+console.log(staggeredCase('ALL_CAPS'));                     // "AlL_CaPs"
+console.log(staggeredCase('ignore 77 the 4444 numbers'));   // "IgNoRe 77 ThE 4444 nUmBeRs"
--- 101/exercices/StringProcessing8/7.js
+++ 101/exercices/StringProcessing8/7.js
@@ -1,8 +1,32 @@
 /* Modify the function from the previous exercise so it ignores non-alphabetic characters when determining whether it should uppercase or lowercase each letter. The non-alphabetic characters should still be included in the return value; they just don't count when toggling the desired case. */
 
-console.log(staggeredCase("I Love Launch School!") === "I lOvE lAuNcH sChOoL!");
-console.log(staggeredCase("ALL CAPS") === "AlL cApS");
+function staggeredCase(txt) {
+  let arr = txt.split('');
+  console.log(arr);
+  return arr.map((el, idx) => {
+    let changeIdx;
+    changeIdx = /A-Za-Z/.test(el)  idx: changeIdx - 1;
+    console.log(changeIdx);
+
+    if (changeIdx % 2 !== 0) {
+      if (/[A-Z]/.test(el)) {
+        return el.toLowerCase();
+      } else {
+        return el;
+      }
+    } else {
+      if (/[a-z]/.test(el)) {
+        return el.toUpperCase();
+      } else {
+        return el;
+      }
+    }
+  }).join('')
+}
+
+// console.log(staggeredCase("ALL CAPS"));
 console.log(
   staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"
 );
-
+console.log(
+  staggeredCase("ignore 77 the 444 numbers"))
