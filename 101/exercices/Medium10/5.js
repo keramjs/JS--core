2018b7f6c014a8bcd33692ec37d0153ae38c7986
keramjs <keramjs@gmail.com>
2022-09-23 13:00:28 +0200

    exercices

--- 101/exercices/Medium10/5.js
+++ 101/exercices/Medium10/5.js
@@ -4,15 +4,58 @@ Write a function that takes an integer as an argument and returns the next featu
 
 NOTE: The largest possible featured number is 9876543201. */
 
-featured(12);           // 21
-featured(20);           // 21
-featured(21);           // 35
-featured(997);          // 1029
-featured(1029);         // 1043
-featured(999999);       // 1023547
-featured(999999987);    // 1023456987
-featured(9876543186);   // 9876543201
-featured(9876543200);   // 9876543201
-featured(9876543201);   // "There is no possible number that fulfills those requirements."
+/* 
+featured num%2 =1 && num%7=0 digits in num can't repeat
+input integer
+outpout next featured num > num
 
+what is closest number to num % 7 ? round up
+check if this number is odd and check if this number has unique digits
+if yes return this number 
+if not add 7 to number and check again
+add only up to 9876543210
+*/
+function featured(num) {
+  let closest = Math.trunc(num / 7) * 7 + 7;
+  while (closest < 9876543210) {
+    if (closest % 2 && areDigitsUnique(closest)) {
+      return closest;
+    }
+    closest += 7;
+  }
+  return "There is no possible number that fulfills those requirements.";
+}
+function extracttDigits(num) {
+  let digits = [];
+  let counter = 1;
+  while (num > 0) {
+    let digit = num % 10;
+    digits.push(digit);
+    num = Math.trunc(num / 10);
+    counter += 1;
+  }
+  return digits;
+}
 
+function areDigitsUnique(num) {
+  let arr = extracttDigits(num);
+  let obj = {};
+  for (let i = 0; i < arr.length; i += 1) {
+    if (obj[arr[i]] === arr[i]) {
+      return false;
+    }
+    obj[arr[i]] = arr[i];
+  }
+  return true;
+}
+
+console.log(featured(12)); // 21
+console.log(featured(20)); // 21
+console.log(featured(21)); // 35
+console.log(featured(997)); // 1029
+console.log(featured(1029)); // 1043
+console.log(featured(999999)); // 1023547
+console.log(featured(999999987)); // 1023456987
+console.log(featured(9876543186)); // 9876543201
+console.log(featured(9876543200)); // 9876543201
+console.log(featured(9876543201)); // "There is no possible number that fulfills those requirements."
--- 101/exercices/Medium10/6.js
+++ 101/exercices/Medium10/6.js
@@ -1,6 +1,21 @@
 /* Write a function that computes the difference between the square of the sum of the first count positive integers and the sum of the squares of the first count positive integers. */
 
-sumSquareDifference(3);      // 22 --> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
-sumSquareDifference(10);     // 2640
-sumSquareDifference(1);      // 0
-sumSquareDifference(100);    // 25164150
+function sumSquareDifference(num){
+    let sqOfSum = 0;
+    for (let i = 1; i <= num; i +=1){
+        sqOfSum += i;
+    }
+    sqOfSum **= 2;
+
+    let sumOfSq = 0;
+    for (let i = 1; i <= num; i +=1){
+        sumOfSq = sumOfSq + i**2;
+    }
+    return sqOfSum-sumOfSq
+}
+
+
+console.log(sumSquareDifference(3));      // 22 --> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+console.log(sumSquareDifference(10));     // 2640
+console.log(sumSquareDifference(1));      // 0
+console.log(sumSquareDifference(100));    // 25164150
