7ce2a3e1f73fa08914b617f895d68d2d8bf3f572
Marek Bajsarowicz <keramjs@gmail.com>
2022-09-26 21:20:50 +0200

    exercices

--- 101/exercices/Advanced11/4.js
+++ 101/exercices/Advanced11/4.js
@@ -33,20 +33,38 @@ Write a function that takes an arbitrary MxN matrix, rotates it clockwise by 90-
 
 
 let matrix1 = [
-    [1, 5, 8],
-    [4, 7, 2],
-    [3, 9, 6],
-  ];
-  
-  let matrix2 = [
-    [3, 7, 4, 2],
-    [5, 1, 0, 8],
-  ];
-  
-  let newMatrix1 = rotate90(matrix1);
-  let newMatrix2 = rotate90(matrix2);
-  let newMatrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))));
-  
-  console.log(newMatrix1);      // [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
-  console.log(newMatrix2);      // [[5, 3], [1, 7], [0, 4], [8, 2]]
-  console.log(newMatrix3);      // `matrix2` --> [[3, 7, 4, 2], [5, 1, 0, 8]]
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6],
+];
+
+let matrix2 = [
+  [3, 7, 4, 2],
+  [5, 1, 0, 8],
+];
+
+function rotate90(mtx) {
+  mtx = mtx.map(el => el.slice());
+  mtxCpy = [];
+  for (let i = 0; i < mtx[0].length; i += 1) {
+    mtxCpy.push([])
+  }
+  console.log(mtx.length);
+  mtxCpy
+  for (let r = 0; r < mtx.length; r += 1) {
+    for (let c = 0; c < mtx[0].length; c += 1) {
+      mtxCpy[c][mtx.length - (r + 1)] = mtx[r][c];
+    }
+  }
+  mtxCpy
+  return mtxCpy
+}
+
+
+let newMatrix1 = rotate90(matrix1);
+let newMatrix2 = rotate90(matrix2);
+let newMatrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))));
+
+console.log(newMatrix1);      // [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+console.log(newMatrix2);      // [[5, 3], [1, 7], [0, 4], [8, 2]]
+console.log(newMatrix3);      // `matrix2` --> [[3, 7, 4, 2], [5, 1, 0, 8]]
--- 101/exercices/Advanced11/5.js
+++ 101/exercices/Advanced11/5.js
@@ -3,8 +3,33 @@
 You may not provide any solution that requires you to sort the result array. You must build the result array one element at a time in the proper order.
 
 Your solution should not mutate the input arrays. */
+function merge(arr1, arr2) {
+  let maxLength = (arr1.length > arr2.length) ? arr1.length : arr2.length;
+  arr1 = arr1.slice();
+  arr2 = arr2.slice();
+  let mergeArr = []
+  for (let i = 0; i < maxLength; i += 1) {
+    if (!arr1[i] && arr2[i]) {
+      mergeArr.push(arr2[i]);
+      continue;
+    }
+    if (arr1[i] && !arr2[i]) {
+      mergeArr.push(arr1[i]);
+      continue;
+    }
+    if (arr1[i] > arr2[i]) {
+      mergeArr.push(arr2[i]);
+      mergeArr.push(arr1[i]);
+    } else {
+      mergeArr.push(arr1[i]);
+      mergeArr.push(arr2[i]);
+    }
+  }
+  return mergeArr;
+}
 
-merge([1, 5, 9], [2, 6, 8]);      // [1, 2, 5, 6, 8, 9]
-merge([1, 1, 3], [2, 2]);         // [1, 1, 2, 2, 3]
-merge([], [1, 4, 5]);             // [1, 4, 5]
-merge([1, 4, 5], []);             // [1, 4, 5]
+console.log(merge([1, 5, 9], [2, 6, 8]));      // [1, 2, 5, 6, 8, 9]
+console.log(merge([1, 1, 3], [2, 2]));         // TODO: to jest cos czego nie wzialem pod uwage[1, 1, 2, 2, 3]
+console.log(merge([], [1, 4, 5]));             // [1, 4, 5]
+console.log(merge([1, 4, 5], []));             // [1, 4, 5]
+
