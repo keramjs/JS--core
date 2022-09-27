782a8c5efa3dc2ab219bfd282982c6d2d6c308eb
Marek Bajsarowicz <keramjs@gmail.com>
2022-09-27 16:26:16 +0200

    exercices

--- 101/exercices/Advanced11/5.js
+++ 101/exercices/Advanced11/5.js
@@ -1,35 +1,44 @@
+const { memoryUsage } = require("process");
+
 /* Write a function that takes two sorted arrays as arguments and returns a new array that contains all the elements from both input arrays in sorted order.
 
 You may not provide any solution that requires you to sort the result array. You must build the result array one element at a time in the proper order.
 
 Your solution should not mutate the input arrays. */
 function merge(arr1, arr2) {
-  let maxLength = (arr1.length > arr2.length) ? arr1.length : arr2.length;
+  let mergeArrMaxLen = arr1.length + arr2.length;
   arr1 = arr1.slice();
   arr2 = arr2.slice();
-  let mergeArr = []
-  for (let i = 0; i < maxLength; i += 1) {
-    if (!arr1[i] && arr2[i]) {
-      mergeArr.push(arr2[i]);
-      continue;
+  let mergeArr = [];
+  let arg1 = arr1.shift();
+  let arg2 = arr2.shift();
+
+  while (mergeArr.length < mergeArrMaxLen) {
+
+    while (arg1 <= arg2) {
+      mergeArr.push(arg1);
+      arg1 = arr1.shift();
     }
-    if (arr1[i] && !arr2[i]) {
-      mergeArr.push(arr1[i]);
-      continue;
+    while (arg1 >= arg2) {
+      mergeArr.push(arg2);
+      arg2 = arr2.shift();
     }
-    if (arr1[i] > arr2[i]) {
-      mergeArr.push(arr2[i]);
-      mergeArr.push(arr1[i]);
-    } else {
-      mergeArr.push(arr1[i]);
-      mergeArr.push(arr2[i]);
+    if (!arg1) {
+      mergeArr.push(arg2);
+      arg2 = arr2.shift();
     }
+    if (!arg1 && !arg2) break;
+    if (!arg2) {
+      mergeArr.push(arg1);
+      arg1 = arr1.shift();
+    }
+
   }
   return mergeArr;
 }
 
 console.log(merge([1, 5, 9], [2, 6, 8]));      // [1, 2, 5, 6, 8, 9]
-console.log(merge([1, 1, 3], [2, 2]));         // TODO: to jest cos czego nie wzialem pod uwage[1, 1, 2, 2, 3]
+console.log(merge([1, 1, 3], [2, 2]));         // [1, 1, 2, 2, 3]
 console.log(merge([], [1, 4, 5]));             // [1, 4, 5]
 console.log(merge([1, 4, 5], []));             // [1, 4, 5]
 
