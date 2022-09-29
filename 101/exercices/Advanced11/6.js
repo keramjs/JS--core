792e7d4c44318b055e96a5291729daa79e2ed949
Marek Bajsarowicz <keramjs@gmail.com>
2022-09-29 14:59:37 +0200

    exercices

--- 101/exercices/Advanced11/6.js
+++ 101/exercices/Advanced11/6.js
@@ -15,15 +15,14 @@ We then work our way back to a flat array by merging each pair of nested subarra
 Write a function that takes an array argument and returns a new array that contains the values from the input array in sorted order. The function should sort the array using the merge sort algorithm as described above. You may assume that every element of the array will be of the same data type—either all numbers or all strings.
 
 Feel free to use the merge function you wrote in the previous exercise. */
-function mergeSort(arr, l, r) {
+function mergeSort(arr) {
   if (arr.length < 2) {
     return arr
   }
   let mid = Math.floor(arr.length / 2);
-  let left = arr.slice(0, mid);
-  mergeSort(arr, 0, mid)
-
-
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid))
+  return mergeSorted(left, right);
 }
 
 function mergeSorted(arr1, arr2) {
@@ -37,11 +36,11 @@ function mergeSorted(arr1, arr2) {
 }
 
 console.log(mergeSort([9, 5, 7, 1]));           // [1, 5, 7, 9]
-// mergeSort([5, 3]);                 // [3, 5]
-// mergeSort([6, 2, 7, 1, 4]);        // [1, 2, 4, 6, 7]
+console.log(mergeSort([5, 3]));                 // [3, 5]
+mergeSort([6, 2, 7, 1, 4]);        // [1, 2, 4, 6, 7]
 
-// mergeSort(['Sue', 'Pete', 'Alice', 'Tyler', 'Rachel', 'Kim', 'Bonnie']);
-// // ["Alice", "Bonnie", "Kim", "Pete", "Rachel", "Sue", "Tyler"]
+console.log(mergeSort(['Sue', 'Pete', 'Alice', 'Tyler', 'Rachel', 'Kim', 'Bonnie']));
+// ["Alice", "Bonnie", "Kim", "Pete", "Rachel", "Sue", "Tyler"]
 
-// mergeSort([7, 3, 9, 15, 23, 1, 6, 51, 22, 37, 54, 43, 5, 25, 35, 18, 46]);
-// [1, 3, 5, 6, 7, 9, 15, 18, 22, 23, 25, 35, 37, 43, 46, 51, 54]
+console.log(mergeSort([7, 3, 9, 15, 23, 1, 6, 51, 22, 37, 54, 43, 5, 25, 35, 18, 46]));
+[1, 3, 5, 6, 7, 9, 15, 18, 22, 23, 25, 35, 37, 43, 46, 51, 54]
