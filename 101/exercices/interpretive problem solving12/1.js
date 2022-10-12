7548bd14c5e475a82de121dc026e483e4ad28145
Marek Bajsarowicz <keramjs@gmail.com>
2022-10-12 23:03:19 +0200

    exercices

--- 101/exercices/interpretive problem solving12/1.js
+++ 101/exercices/interpretive problem solving12/1.js
@@ -4,15 +4,37 @@ Write a program that takes one argument—the total number of switches—and ret
 
 
 function lightsOn(switches) {
-    // ...
+  let arr = [];
+  for (let i = 0; i < switches; i += 1) {
+    arr[i] = 1;
   }
-  
-  lightsOn(5);        // [1, 4]
-  // Detailed result of each round for `5` lights
-  // Round 1: all lights are on
-  // Round 2: lights 2 and 4 are now off;     1, 3, and 5 are on
-  // Round 3: lights 2, 3, and 4 are now off; 1 and 5 are on
-  // Round 4: lights 2 and 3 are now off;     1, 4, and 5 are on
-  // Round 5: lights 2, 3, and 5 are now off; 1 and 4 are on
-  
-  lightsOn(100);      // [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+  let step = 2;
+  let count = switches - 1;
+  while (count) {
+    arr = arr.map((el, idx) => {
+      if (!((idx + 1) % step)) {
+        return el ? 0 : 1;
+      }
+      return el
+    });
+    step += 1;
+    count -= 1;
+  }
+
+  let lightsOn = [];
+  for (let i = 0; i < switches; i += 1) {
+    if (arr[i] === 1) lightsOn.push(i + 1);
+  }
+  return lightsOn;
+}
+
+console.log(lightsOn(5));        // [1, 4]
+// Detailed result of each round for `5` lights
+// Round 1: all lights are on
+// Round 2: lights 2 and 4 are now off;     1, 3, and 5 are on
+// Round 3: lights 2, 3, and 4 are now off; 1 and 5 are on
+// Round 4: lights 2 and 3 are now off;     1, 4, and 5 are on
+// Round 5: lights 2, 3, and 5 are now off; 1 and 4 are on
+
+console.log(lightsOn(100));      // [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
